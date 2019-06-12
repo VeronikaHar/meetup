8262c3356b1f065264e7184e395777d9e002e23e
veronikahar <veronika.har@hotmail.com>
2019-06-12 17:00:33 -0500

    Add styling.

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -3,15 +3,17 @@ import React, { Component } from "react";
 class NumberOfEvents extends Component {
     state = {
         eventNumber: 32,
-    }
+    };
+
     handleChange = (event) => {
         const value = event.target.value;
         this.setState({ eventNumber: value });
-    }
+    };
+
     render() {
         return (
             <div className="NumberOfEvents">
-                Show <input type="number" placeholder="32" value={this.state.eventNumber} onChange={this.handleChange}></input> events
+                Show <input className="number-of-events" type="number" placeholder="32" value={this.state.eventNumber} onChange={this.handleChange}></input> events
             </div>
         );
     }
