c7c69b0026378a4bd353346327e6735b57d79913
veronikahar <veronika.har@hotmail.com>
2019-06-18 21:24:20 -0500

    Add info and error alerts.

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import { getSuggestions } from './api';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
     state = {
-        query: '',
+        query: "",
         suggestions: [],
+        infoText: ""
     }
 
     handleInputChanged = (event) => {
         const value = event.target.value;
         this.setState({ query: value });
-        getSuggestions(value).then(suggestions => this.setState({ suggestions }));
-    };
+        getSuggestions(value).then(suggestions => {
+            this.setState({ suggestions });
+
+            if (value && suggestions.length === 0) {
+                this.setState({
+                    infoText: "We cannot find the city you are looking for. Please try another city.",
+                });
+            } else {
+                this.setState({ infoText: "" });
+            }
+        });
+    }
     handleItemClicked = (value, lat, lon) => {
         this.setState({ query: value, suggestions: [] });
         this.props.updateEvents(lat, lon);
@@ -22,6 +34,7 @@ class CitySearch extends Component {
             <div className="CitySearch">
                 <input type="text" className="city"
                     value={this.state.query} onChange={this.handleInputChanged} placeholder="Search a city..." />
+                <InfoAlert text={this.state.infoText} />
                 <ul className="suggestions">
                     {this.state.suggestions.map(item =>
                         <li key={item.name_string} onClick={() => this.handleItemClicked(item.name_string, item.lat, item.lon)}>{item.name_string}</li>
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,20 +1,29 @@
 import React, { Component } from "react";
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
     state = {
         eventNumber: 32,
+        errorText: ""
     };
 
     handleChange = (event) => {
         const value = event.target.value;
         this.setState({ eventNumber: value });
         this.props.updateEvents(null, null, value);
+
+        if (value < 1) {
+            this.setState({ errorText: "Event number should be at least 1." })
+        } else {
+            this.setState({ errorText: "" });
+        }
     };
 
     render() {
         return (
             <div className="NumberOfEvents">
                 Show <input className="number-of-events" type="number" value={this.state.eventNumber} onChange={this.handleChange}></input> events
+                <ErrorAlert text={this.state.errorText} />
             </div>
         );
     }
