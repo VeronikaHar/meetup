0c5399e547fb3b04c082cc5af4b20a0210ff1206
VeronikaHar <veronika.har@hotmail.com>
2019-06-19 22:24:25 -0500

    Updates

--- precache-manifest.b51a2da78970f3e43f73d2e2a65cd49d.js
+++ precache-manifest.b51a2da78970f3e43f73d2e2a65cd49d.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5976823eda6467fbc989304e8918e9c0",
+    "revision": "f04d4b6c4c7ac4b147957b69da3394cb",
     "url": "/meetup/index.html"
   },
   {
-    "revision": "fa9b8852bd21622e7c72",
+    "revision": "6fd7799e9fd185d9d377",
     "url": "/meetup/static/css/main.85c862ff.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/meetup/static/js/2.db0827aa.chunk.js"
   },
   {
-    "revision": "fa9b8852bd21622e7c72",
-    "url": "/meetup/static/js/main.c424e7e0.chunk.js"
+    "revision": "6fd7799e9fd185d9d377",
+    "url": "/meetup/static/js/main.6a57cb69.chunk.js"
   },
   {
     "revision": "c81de03e5fcc2eb84eed",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/meetup/precache-manifest.89c8ca41dab7a5f8ae4b58304e90c557.js"
+  "/meetup/precache-manifest.b51a2da78970f3e43f73d2e2a65cd49d.js"
 );
 
 self.addEventListener('message', (event) => {
